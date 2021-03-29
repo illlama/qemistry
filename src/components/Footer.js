7410050e356c25227dbebbf5315b8542d4796c6e
illlama <beatlemaniakih@gmail.com>
2021-03-29 13:38:39 +0900

    footer css edit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from 'scss/components/Footer.module.scss';
 import Telegram from 'assets/Telegram.png';
 import Medium from 'assets/Medium.png';
 import Twitter from 'assets/Twitter.png';
@@ -7,14 +6,36 @@ import Youtube from 'assets/Youtube.png';
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      &copy; Copyright {new Date().getFullYear()} Qemistry. All rights reserved.
-      <div>
-        <a href="https://www.medium.com">
-          <img src={Telegram} alt="spcial-telegram" />
-          <img src={Medium} alt="spcial-medium" />
-          <img src={Twitter} alt="spcial-twitter" />
-          <img src={Youtube} alt="spcial-youtube" />
+    <div className="flex flex-wrap grid grid-cols-3 content-center h-48 w-full bg-mainGray">
+      <div className="col-span-1"></div>
+      <p className="col-span-1 text-white text-center font-light text-sm tracking-wider">
+        &copy; Copyright {new Date().getFullYear()} Qemistry. All rights
+        reserved.
+      </p>
+      <div className="col-span-1 justify-center flex space-x-4">
+        <a
+          href="https://www.medium.com"
+          className="hover:bg-mainBlack rounded-lg p-1"
+        >
+          <img src={Telegram} alt="social-telegram" />
+        </a>
+        <a
+          href="https://www.medium.com"
+          className="hover:bg-mainBlack rounded-lg p-1"
+        >
+          <img src={Medium} alt="social-medium" />
+        </a>
+        <a
+          href="https://www.medium.com"
+          className="hover:bg-mainBlack rounded-lg p-1"
+        >
+          <img src={Twitter} alt="social-twitter" />
+        </a>
+        <a
+          href="https://www.medium.com"
+          className="hover:bg-mainBlack rounded-lg p-1"
+        >
+          <img src={Youtube} alt="social-youtube" />
         </a>
       </div>
     </div>
